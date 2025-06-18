363f762d8df4b1c69c95428be3f2c215087e4842
Shubham <shubhamjoshi281@gmail.com>
2025-06-18 22:24:05 +0530

    added automation of 14 min

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
+import job from "./config/cron.js";
 
 const app = express();
 
 // middleware
+job.start();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
